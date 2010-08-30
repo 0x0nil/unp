1a7a3426d72d08f33433032aa667c35d42ebc711
liang <liang.hsh@gmail.com>
2010-08-31 05:51:47 +0800

    1 在c文件中加入在书中图的标号，到时查找起来方便 2 修改TCP时间获取客户程序

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -1,21 +1,60 @@
+/*
+ * 图1-5 TCP时间获取客户程序
+ * */
 #include <stdio.h>
-#include "unp.h"
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main(int argc,char argv[])
+#define MAXLINE 521
+
+int main(int argc,char *argv[])
 {
     int sockfd,n;
     char recvline[MAXLINE +1];
     struct sockaddr_in servaddr;
     if(argc != 2)
     {
-        err_quit("usage:a.out<IP address>");
-        if((sockfd=socket(AF_INET,SOCK_STREAM,0)) <0)
-            err_sys("socket error");
-
-        bzero(&servaddr,sizeof(servaddr));
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_port = htons(13); /*daytime server */
-        if(inet_pton(AF_INET,argv[1],&servaddr.sin_addr) <=0)
-            err_quit("inet_pton error");
-        return 0;
+        perror("usage:a.out<IP address>");
+        exit(0);
     }
+
+    if((sockfd=socket(AF_INET,SOCK_STREAM,0)) <0)
+    {
+        perror("socket error");
+        exit(0);
+    }
+
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(13); /*daytime server */
+    if(inet_pton(AF_INET,argv[1],&servaddr.sin_addr) <=0)
+    {
+        perror("inet_pton error");
+        exit(0);
+    }
+
+    if(connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) < 0)
+    {
+        perror("connect error");
+        exit(0);
+    }
+
+    while( (n = read(sockfd,recvline,MAXLINE)) > 0)
+    {
+        recvline[n] = 0;
+        if(fputs(recvline,stdout) == EOF)
+        {
+            perror("fputs error");
+            exit(0);
+        }
+    }
+    if(n < 0)
+    {
+        perror("read error");
+        exit(0);
+    }
+
+    exit(0);
+}
