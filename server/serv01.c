de981e281cce603a10347308c73b5ae873e2b6f1
liang <liang.hsh@gmail.com>
2010-09-03 14:59:51 +0800

    修改了一下serv01

--- server/serv01.c
+++ server/serv01.c
@@ -3,6 +3,12 @@
  * */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <errno.h>
+#include <signal.h>
+#include "../lib/tcp_listen.h"
 
 int
 main(int argc, char *argv[])
@@ -10,7 +16,7 @@ main(int argc, char *argv[])
     int listenfd;
     int connfd;
     pid_t pid;
-    void sig_child(int);
+    void sig_chld(int);
     void sig_init(int);
     void web_clild(int);
 
@@ -20,4 +26,54 @@ main(int argc, char *argv[])
 
     if(argc == 2)
         tcp_listen(NULL,argv[1],&addrlen);
+    else if(argc == 3)
+        tcp_listen(argv[1],argv[2],&addrlen);
+    else
+    {
+        fprintf(stdout,"usage:serv01 [<host>] <port#>\n");
+        exit(1);
+    }
+
+    cliaddr = (struct sockaddr *)malloc(addrlen);
+    if(cliaddr == NULL)
+    {
+        perror("malloc error");
+        exit(1);
+    }
+
+    signal(SIGCHLD,sig_chld);
+    signal(SIGINT,sig_int);
+
+    for(; ;)
+    {
+        clilen = addrlen;
+
+        if((connfd = accpt(listenfd,cliaddr,&clilen)) < 0)
+        {
+            if(errno == EINTR)
+                continue;    /* back to for() */
+            else
+            {
+                perror("accpt error");
+                exit(1);
+            }
+        }
+
+        if((childpid = fork()) == 0)
+        {
+            /* child process */
+            close(listenfd);             /* close listening socket */
+            web_child(connfd);           /* process the request */
+            exit(0);
+        }
+
+        close(connfd);                  /* parent closes connected socket */
+    }
+}
+
+void sig_int(int signo)
+{
+    void pr_cpu_time(void);
+    pr_cpu_time();
+    exit(0);
 }
